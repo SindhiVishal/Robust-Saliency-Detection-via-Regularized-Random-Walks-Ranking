6259c5d4924b4f736fa74a4892f78bdf05a6d607
SindhiVishal <98824334+SindhiVishal@users.noreply.github.com>
2026-07-01 02:24:43 +0530

    Update slic.py

--- robust_saliency/src/segmentation/slic.py
+++ robust_saliency/src/segmentation/slic.py
@@ -1,1 +1,40 @@
+from skimage.segmentation import slic
 
+from .base import SuperpixelGenerator
+
+
+class SLICGenerator(SuperpixelGenerator):
+
+    def __init__(
+
+        self,
+
+        n_segments=300,
+
+        compactness=10,
+
+        sigma=1,
+
+    ):
+
+        self.n_segments = n_segments
+
+        self.compactness = compactness
+
+        self.sigma = sigma
+
+    def generate(self, image):
+
+        return slic(
+
+            image,
+
+            n_segments=self.n_segments,
+
+            compactness=self.compactness,
+
+            sigma=self.sigma,
+
+            start_label=0,
+
+        )
