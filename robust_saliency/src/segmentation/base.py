9d07ceb0f2d99b1419fa709959611ad3923a96a0
SindhiVishal <98824334+SindhiVishal@users.noreply.github.com>
2026-07-01 02:23:44 +0530

    Update base.py

--- robust_saliency/src/segmentation/base.py
+++ robust_saliency/src/segmentation/base.py
@@ -1,1 +1,14 @@
+from abc import ABC
+from abc import abstractmethod
 
+import numpy as np
+
+
+class SuperpixelGenerator(ABC):
+
+    @abstractmethod
+    def generate(
+        self,
+        image: np.ndarray,
+    ) -> np.ndarray:
+        pass
